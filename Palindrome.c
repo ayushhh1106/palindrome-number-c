a949946f55e13ad72663ee744ccaa457d9630acc
Ayush Kumar <ayushhh.1106@gmail.com>
2025-12-30 14:43:43 +0530

    MINI PROJECT
    Added comments for clarity and author information.

--- Palindrome.c
+++ Palindrome.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    //Ayush Kumar 10343
     int num, original, reversed = 0, digit;
 
-    // Taking input
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Store original number
     original = num;
 
-    // Reverse the number
     while (num != 0) {
         digit = num % 10;
         reversed = reversed * 10 + digit;
         num = num / 10;
     }
 
-    // Check palindrome
     if (original == reversed) {
         printf("Palindrome Number\n");
     } else {

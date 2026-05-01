1ab8213178a7b1e55dbd33fd51872e8ac3cb9801
Ayush Kumar <ayushhh.1106@gmail.com>
2026-05-01 12:32:56 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -5,7 +5,7 @@ int main() {
     // sec e
     int num, original, reversed = 0, digit;
 
-    printf("Enter a number: ");
+    printf("Enter a digit: ");
     scanf("%d", &num);
 
     original = num;

3a431bd8166a13bb9597156f00327b1fcb0ef043
Ayush Kumar <ayushhh.1106@gmail.com>
2026-06-13 11:57:30 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -19,7 +19,7 @@ int main() {
     if (original == reversed) {
         printf("Palindrome Number \n");
     } else {
-        printf("Not a Palindrome no\n");
+        printf("Not a Palindrome \n");
     }
 
     return 0;
